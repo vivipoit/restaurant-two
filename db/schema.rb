8da6ff979e0dc7ebcdc8523174885da31e417368
ViviPoit <vwpoit@gmail.com>
2019-03-05 10:35:13 +0630

    scaffold Client

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_035728) do
+ActiveRecord::Schema.define(version: 2019_03_05_040321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.integer "number_of_people"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tables", force: :cascade do |t|
     t.integer "number"
